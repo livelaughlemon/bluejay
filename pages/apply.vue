46bfa6e74b5ed2ee239e0b2668f893063184869c
livelaughlemon <harperssademailaddress@gmail.com>
2023-06-11 21:28:42 -0400

    Embeds application into website

---
+++ pages/apply.vue
@@ -0,0 +1,21 @@
+<script setup>
+import PageHeader from '../components/PageHeader.vue'
+import PageFooter from '../components/PageFooter.vue'
+</script>
+
+
+<template>
+  <div>
+    <div class="pageHeader noImage">
+      <PageHeader />
+      <div class="pageHeaderContent">
+        <div class="pageHeaderItemContainer">
+            <!-- Don't steal our form... that would be so weird -->
+            <iframe src="https://tally.so/r/3E50EA" width="100%" height="100%" style="height: 1600px; border-radius: 10px;" allowtransparency="true" frameborder="0" marginheight="0" marginwidth="0" title="Join the Orchid Staff Team"></iframe>
+        </div>
+      </div>
+    </div>
+    <PageFooter />
+  </div>
+  <NuxtPage />
+</template>
